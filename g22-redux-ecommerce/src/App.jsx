bddbc618049e21b1d0e6a7e9aafd2d2fcbf6cafd
Victor Villazon <victorzn23@gmail.com>
2023-08-31 21:09:16 -0500

    terminando maquetación del producto

--- g22-redux-ecommerce/src/App.jsx
+++ g22-redux-ecommerce/src/App.jsx
@@ -2,6 +2,8 @@ import iconMenu from './assets/images/icon-menu.svg'
 import iconCart from './assets/images/icon-cart.svg'
 import iconAvatar from './assets/images/image-avatar.png'
 import imageProduct from './assets/images/image-product-1.jpg'
+import iconMinus from './assets/images/icon-minus.svg'
+import iconPlus from './assets/images/icon-plus.svg'
 
 function App() {
   return (
@@ -23,25 +25,26 @@ function App() {
         <section className='w-full bg-red-500'>
           <img src={imageProduct} className='h-[500px] w-full object-cover' />
         </section>
-        <section>
-          <h3>Sneaker company</h3>
-          <h1>Fall Limited Edition Sneakers</h1>
-          <p>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.</p>
-          <div>
-            <div>
-              <h2>$125.00</h2>
-              <span>50%</span>
+        <section className='p-8 flex flex-col gap-4'>
+          <h3 className='text-xl uppercase font-bold text-[#ff7d1b]'>Sneaker company</h3>
+          <h1 className='text-5xl font-bold'>Fall Limited Edition Sneakers</h1>
+          <p className='text-xl font-normal text-gray-500'>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.</p>
+          <div className='flex justify-between'>
+            <div className='flex gap-6'>
+              <h2 className='text-5xl font-bold'>$125.00</h2>
+              <span className='text-2xl font-bold text-[#ff7d1b] bg-[#ff7d1b]/20 px-4 py-2 rounded-lg'>50%</span>
             </div>
-            <div>
+            <div className='text-2xl font-bold line-through text-gray-500'>
               $250.00
             </div>
           </div>
-          <div>
-            <img src={} />
-            <span>0</span>
-            <img src={} />
+          <div className='flex justify-between items-center bg-gray-200 px-8 py-6 rounded-xl'>
+            <img src={iconMinus} />
+            <span className='text-2xl font-bold'>0</span>
+            <img src={iconPlus} />
           </div>
-          <button>
+          <button className='w-full text-2xl font-bold p-6 text-white rounded-xl flex justify-center gap-6 bg-[#ff7d1b] hover:bg-[#ff7d1b]/80 '>
+            <img src={iconCart} className='mt-1' />
             Add to cart
           </button>
         </section>
